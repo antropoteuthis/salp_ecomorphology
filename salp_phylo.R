1e6d5b983e14a6a8b7f90ba8ceaac06a118e2268
antropoteuthis <alejandro.damianserrano@yale.edu>
2021-11-04 17:27:36 -0700

    phylo comparative code

--- salp_phylo.R
+++ salp_phylo.R
@@ -8,19 +8,52 @@ traits <- read.csv("salplit.tsv", sep="\t", stringsAsFactors = F)
 
 ggplot(traits[which(traits$Variable=="Mean swimming speed cms"),], aes(x = Species, y = Value)) + geom_point() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-ggplot(traits[which(traits$Variable=="Pulsation rate Hz"),], aes(x = Species, y = Value)) + geom_point() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+ggplot(traits[which(traits$Variable=="Mean pulsation rate Hz"),], aes(x = Species, y = Value)) + geom_point() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
 ggplot(traits[which(traits$Variable=="Cost of locomotion J-kg-m"),], aes(x = Species, y = Value)) + geom_point() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
-tree_salp <- read.newick("salpML.tre")
-tree_salp <- rtree(length(unique(traits$Species))) ## BULLSHIT
-tree_salp$tip.label <- unique(traits$Species) ## BULLSHIT
+tree_salp <- read.newick("phylogeny/salps18Saligned.fa.treefile")
+tree_salp$tip.label <- str_remove_all(tree_salp$tip.label, "HQ.+?_")
+tree_salp$tip.label <- str_replace_all(tree_salp$tip.label, "_", " ")
+tree_salp <- reroot(tree_salp, 55) #reroot
+tree_salp <- drop.tip(tree_salp, 32:39) #drop outgroups
+tree_salp <- drop.tip(tree_salp, c(29,24,26,17,21,2,3,6,7,8,13,19)) #drop duplicate species
+tree_salp$tip.label[which(tree_salp$tip.label == "Iasis cylindrica")] <- "Iasis (Weelia) cylindrica"
 
-tree_salp_pruned <- drop.tip(tree_salp, which(!(tree_salp$tip.label %in% unique(traits$Species))))
+#prune tree by data and make ultrametric
+tree_salp_pruned <- drop.tip(tree_salp, which(!(tree_salp$tip.label %in% unique(traits$Species)))) %>% chronos()
+
 pruned_traits <- traits[which(traits$Species %in% tree_salp_pruned$tip.label),]
 
-cast_num <- dcast(traits[which(traits$Class=="number"),], Species~Variable, value.var="Value", fun.aggregate = function(x){mean(as.numeric(x), na.rm = T)})
+cast_num <- dcast(pruned_traits[which(pruned_traits$Class=="number"),], Species~Variable, value.var="Value", fun.aggregate = function(x){mean(as.numeric(x), na.rm = T)})
+#cast_num[is.na(cast_num)] <- 0 #CAREFUL!!
 
-cast_cat <- dcast(traits[which(traits$Class=="category"),], Species~Variable, value.var="Value", fun.aggregate = unique)
-morph <- traits[which(traits$Variable=="Chain architecture"),c(1,4)]
-names(morph)[2] <- "Chain architecture"
+dotTree(drop.tip(tree_salp_pruned, which(!(tree_salp_pruned$tip.label %in% names(setNames(as.numeric(cast_num$`Mean swimming speed cms`)[which(!is.na(cast_num$`Mean swimming speed cms`))],cast_num$Species))))),setNames(as.numeric(cast_num$`Mean swimming speed cms`)[which(!is.na(cast_num$`Mean swimming speed cms`))],cast_num$Species))
+#cast_cat <- dcast(traits[which(traits$Class=="category"),], Species~Variable, value.var="Value", fun.aggregate = unique)
+morph <- setNames(pruned_traits[which(pruned_traits$Variable=="Chain architecture"),4], pruned_traits[which(pruned_traits$Variable=="Chain architecture"),1])
+simmorph<-make.simmap(tree_salp_pruned,morph,model="ER",nsim=100)
+par(ask=F)
+obj<-summary(simmorph,plot=FALSE)
+cols<-setNames(palette()[1:6],mapped.states(simmorph)[,1])
+plot(obj,colors=cols,fsize=0.8,cex=c(0.9,0.5), ftype="i")
+add.simmap.legend(colors=cols,x=0, y=4,prompt=FALSE,fsize=0.9,)
+
+for(i in 2:ncol(cast_num)){
+  CH_I=as.numeric(cast_num[,i])
+  names(CH_I) = cast_num$Species
+  CH_I= CH_I[!is.na(CH_I)]
+  treeI = drop.tip(tree_salp_pruned,which(!(tree_salp_pruned$tip.label %in% names(CH_I))))
+  class(treeI) = "phylo"
+  PSIG <- phylosig(treeI, CH_I, test=T)
+  print(names(cast_num)[i])
+  PSIG$K %>% print()
+  PSIG$P %>% print()
+  length(CH_I) %>% print()
+  #dotTree(treeI,CH_I)
+  if(length(CH_I)>4){
+    obj<-contMap(treeI,CH_I,plot=FALSE)
+    grey<-setMap(obj,c("white","black"))
+    par(mfrow=c(1,1))
+    plot(grey,lwd=7,legend=2, leg.txt=names(cast_num)[i], fsize=c(1,0.5))
+  }
+}
